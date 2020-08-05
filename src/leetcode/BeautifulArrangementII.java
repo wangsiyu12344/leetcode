60f4b25a0618b4a9a9c9f5b475ad946433439161
wangsiyu12344 <wangsiyu12344@163.com>
2020-08-05 15:59:39 +0900

    leetcode

--- src/leetcode/BeautifulArrangementII.java
+++ src/leetcode/BeautifulArrangementII.java
@@ -1,5 +1,15 @@
 package leetcode;
 
+/**
+ * 667.优美的排列2
+ *
+ *给定两个整数 n 和 k，你需要实现一个数组，这个数组包含从 1 到 n 的 n 个不同整数，同时满足以下条件：
+ *
+ * ① 如果这个数组是 [a1, a2, a3, ... , an] ，那么数组 [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] 中应该有且仅有 k 个不同整数；.
+ *
+ * ② 如果存在多种答案，你只需实现并返回其中任意一种.
+ *
+ */
 public class BeautifulArrangementII {
     public int[] constructArray(int n, int k) {
         int[] res = new int[n];
