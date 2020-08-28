3ff993490310340549702082eaf2ea97a455acd6
wangsiyu12344 <wangsiyu12344@163.com>
2020-08-28 11:29:54 +0900

    leetcode

---
+++ src/leetcode/SingleNumber.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 136. 只出现一次的数字
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，
+ * 其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ */
+public class SingleNumber {
+    public int SingleNumber(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+}
